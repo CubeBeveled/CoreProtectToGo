60bc2b49f9327efd58e22cb03a77ea8821504184
CubeBeveled <cubebeveled@gmail.com>
2025-04-09 14:40:11 +0200

    Forgot to do something

--- build.sh
+++ build.sh
@@ -3,5 +3,8 @@
 git clone https://github.com/PlayPro/CoreProtect
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
+
 cd CoreProtect
+sed -i 's/branch: ${project.branch}/branch: developement/' CoreProtect/src/main/resources/plugin.yml
+
 mvn clean install
