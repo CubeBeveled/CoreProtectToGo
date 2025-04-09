d5de984101931442f70ad818e490cc59b3ad21b0
CubeBeveled <cubebeveled@gmail.com>
2025-04-09 14:41:17 +0200

    Wrong dir

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
 
 cd CoreProtect
-sed -i 's/branch: ${project.branch}/branch: developement/' CoreProtect/src/main/resources/plugin.yml
+sed -i 's/branch: ${project.branch}/branch: developement/' src/main/resources/plugin.yml
 
 mvn clean install
