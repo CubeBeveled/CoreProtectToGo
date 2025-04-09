6d68bc8e11e71b256e705115481bd78820da21bc
CubeBeveled <cubebeveled@gmail.com>
2025-04-09 14:24:44 +0200

    Changed shi

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-git clone https://github.com/PaperMC/Folia
+git clone https://github.com/PlayPro/CoreProtect
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
-cd Folia
-./gradlew applyPatches
-./gradlew createReobfPaperclipJar
+cd CoreProtect
+mvn clean install
