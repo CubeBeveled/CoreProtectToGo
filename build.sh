b87bed39533eea78736290277eea61f84b5b22a9
Your Name <you@example.com>
2026-05-06 14:01:23 +0200

    remove unnecessary stuff

--- build.sh
+++ build.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
 git clone https://github.com/PlayPro/CoreProtect
-git config --global user.email "you@example.com"
-git config --global user.name "Your Name"
-
 cd CoreProtect
-sed -i 's/branch: ${project.branch}/branch: developement/' src/main/resources/plugin.yml
 
-mvn clean install
+mvn -B verify
